f81362a05c11116f63d894671dc9a27e444fc3f8
Oleg Shirokov <oleeq2@gmail.com>
2013-07-21 19:13:35 +0400

    Test deleted from code

--- sort/insert.c
+++ sort/insert.c
@@ -11,14 +11,3 @@ void insert_sort(int *array,int n)
        array[j+1] = key;
     }
 }
-
-int main(int argc, const char *argv[])
-{
-    int array[5] = { -2, 5 , 0 , 3 , 1};
-    insert_sort(array,5);
-    int i;
-    for (i = 0; i < 5; i++) {
-        printf("%d ", array[i]);
-    }
-    return 0;
-}
