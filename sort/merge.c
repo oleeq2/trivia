3cf1add8a0ffc64aaafee29039187df5d9c3e1d6
Oleg Shirokov <oleeq2@gmail.com>
2013-07-21 15:51:28 +0400

    Fixed merge sort

--- sort/merge.c
+++ sort/merge.c
@@ -1,50 +1,59 @@
+#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 
 void merge(int *array,int l,int med,int r)
 {
     int n_f,n_s;
-    
-    n_f = med - l + 1;
+    int i;
+    int j;
+    n_f = med - l +1; 
     n_s = r - med;
 
-    int *f_array,*s_array;
-    f_array = calloc(n_f+1,sizeof(int));
-    s_array = calloc(n_s+1,sizeof(int));
+    int *f_array = calloc(n_f+1,sizeof(int));
+    int *s_array = calloc(n_s+1,sizeof(int));
 
 
-    memcpy(f_array,array,n_f);
-    memcpy(s_array,array+med+1,n_s);
-        
-    int i;
-    int j=0,k=0;
-    for (i = l; i < r; i++) {
-        if(f_array[j] <= s_array[k])
-        {
-            array[i] = f_array[j];
-            j++;
-        } else {
-            array[i] = s_array[k];
-            k++;
-        }
+    for(i = l,j=0; i <= med;j++,i++){
+        f_array[j] = array[i];
     }
+
+    for(i = med+1,j=0; i <= r; i++,j++){
+        s_array[j] = array[i];
+    }
+
+    f_array[n_f] = INT_MAX;
+    s_array[n_s] = INT_MAX;
+
+    int f=0,s=0;
+    for (i = l; i <= r; i++) {
+        if (f_array[f] <= s_array[s]) {
+           array[i] = f_array[f];
+           f++;
+        }else {
+            array[i] = s_array[s];
+            s++;
+        }
+        printf("%d ",array[i]);
+    }
+    putchar('\n');
 }
 
 void merge_sort(int *array,int l,int r)
 {
-    if (r - l <= 1)
+    if(l < r)
     {
-        return;
-    } else if (r > l) {
-        merge_sort(array,l,r/2);
-        merge_sort(array,(r/2)+1,r);
-        merge(array,l,(r/2),r);
+        int q = (l+r)/2;
+        merge_sort(array,l,q);
+        merge_sort(array,q+1,r);
+        merge(array,l,q,r);
     }
 }
 
 int main(int argc, const char *argv[])
 {
-    int arr[5] = { 12 , 0 ,-3, 4 ,5};
+    int arr[5] = {-12 , 3 ,-1, 4 ,5};
     merge_sort(arr,0,4);
     int i;
     for (i = 0; i < 5; i++) {
