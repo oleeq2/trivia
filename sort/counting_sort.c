8c074d177dd78290aa512701b1215f203c3ede53
Oleg Shirokov <oleeq2@gmail.com>
2013-07-25 15:14:35 +0400

    Counting sort

--- sort/counting_sort.c
+++ sort/counting_sort.c
@@ -1,29 +1,19 @@
-void counting_sort(int *array,int n,int *result_arr,int max_el)
+#include <stdio.h>
+
+void counting_sort(int *array,int *result_array,int n,int max)
 {
-    int temp_array[max_el+1]; 
+    int temp_array[max+1];
     int i;
-    for(i = 0; i <= max_el;i++){
+    for (i = 0;i <= max;i++)
         temp_array[i] = 0;
-    }
-    for(i = 0; i < n; i++){
+
+    for (i = 0;i < n; i++)
         temp_array[array[i]]++;
-    }
-    for(i = 1; i <= max_el;i++){
-        temp_array[i] = temp_array[i-1] + temp_array[i]; 
-    }
 
-    for(i = 0; i < n ; i++){
-        result_arr[temp_array[i]--] = array[i];
-    }
+    for (i = 1; i <= max; i++)
+        temp_array[i] += temp_array[i-1];
+
+    for (i = 0; i < n; i++)
+        result_array[temp_array[array[i]]--] = array[i];
 }
 
-int main(int argc, const char *argv[])
-{
-    int arr[5] = { 1 ,2, 5, 2, 0};
-    int res[5]; 
-    counting_sort(arr,5,res,5);
-    int i;
-    for(i=0;i < 5;i++)
-        printf("%d ",res[i]);
-    return 0;
-}
